1e996993a85f7e2d7ad656679a05cac11447270b
Roshan Poudel <roshan.poudel@stonybrook.edu>
2020-09-06 23:01:45 +0900

    finished week1, qn 1

--- week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -14,17 +14,27 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append((next, i))
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if len(opening_brackets_stack) == 0:
+                return i + 1
+            if not are_matching(opening_brackets_stack.pop()[0], next):
+                return i + 1
+
+    if not (len(opening_brackets_stack) == 0):
+        return opening_brackets_stack[0][1] + 1
+    else:
+        return "Success"
+
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
